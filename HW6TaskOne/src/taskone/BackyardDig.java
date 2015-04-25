5c2e8ba0a7cf9f68e2a4522ec3a57a4ca8cf893a
tjiang11 <tjiang11@jhu.edu>
2015-04-24 22:48:16 -0400

    Added importing of files

--- HW6TaskOne/src/taskone/BackyardDig.java
+++ HW6TaskOne/src/taskone/BackyardDig.java
@@ -1,5 +1,9 @@
 package taskone;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 /**
  * Given a collection of match-cars that lie in a grid, 
  * generate the network of tunnels that will require the 
@@ -24,6 +28,17 @@ public final class BackyardDig {
         // TODO Auto-generated method stub
         System.out.println("This assignment is long as hell.");
         System.out.println("First change.");
+        String fileName = "digInput1.txt";
+        try {
+            FileReader fr = new FileReader(fileName);
+            BufferedReader br = new BufferedReader(fr);
+            br.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error opening file");
+        } catch (IOException e) {
+            System.out.println("IOException");
+        }
+        
     }
 
 }

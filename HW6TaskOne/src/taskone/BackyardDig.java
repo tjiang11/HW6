28cadd26105c38c5b14b893346367797fb1c9391
tjiang11 <tjiang11@jhu.edu>
2015-04-24 21:59:02 -0400

    test

---
+++ HW6TaskOne/src/taskone/BackyardDig.java
@@ -0,0 +1,28 @@
+package taskone;
+
+/**
+ * Given a collection of match-cars that lie in a grid, 
+ * generate the network of tunnels that will require the 
+ * least work the reach every car.
+ * @author Tony Jiang, Devin Sun, Suyi Liu
+ *
+ */
+public final class BackyardDig {
+
+    /**
+     * Shutup Checkstyle.
+     */
+    private BackyardDig() {   
+    }
+    
+    /**
+     * Main method.
+     * @param args First argument: Input file containing 
+     * grid size and car locations based on edges and respective weights.
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("This assignment is long as hell.");
+    }
+
+}
